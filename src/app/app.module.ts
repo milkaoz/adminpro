e357808dbe70e29f32c22ee2d7eedc3391ea85ea
milkaoz <milkaoz@gmail.com>
2020-05-24 09:33:56 -0400

    Modificacion en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,26 @@ import { NgModule } from '@angular/core';
 // Rutas creadas en app.routes.ts
 import { APP_ROUTES } from './app.routes';
 
+// Modulos
+import { PagesModule } from './pages/pages.modules';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagesfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
+
   ],
   // Aqui se agregan los modulos, por ejemplo el modulo que se creo en app.routes con todas las rutas
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProgressComponent } from './pages/progress/progress.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
 import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
-import { PagesComponent } from './pages/pages.component';
 
 const appRoutes: Routes = [
-  // Cuando no existe ruta redirecciona al dashboard
-  {
-    path: '',
-    component: PagesComponent,
-    // rutas hijas de pagesComponent
-    // pages component tiene un component <router-outlet></router-outlet>
-    // que permite cargar componentes (dashboard, progress, graficas1)
-    // dentro de pagesComponent
-    children: [
-      { path: 'dashboard', component: DashboardComponent},
-      { path: 'progress', component: ProgressComponent},
-      { path: 'graficas1', component: Graficas1Component},
 
-      // Cuando no existe ruta redirecciona al dashboard
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
 
-
-
-// Cualquier ruta que no este defina, redireciona a paganotfound
-{ path: '**', component: NopagesfoundComponent}
+  // Cualquier ruta que no este defina, redireciona a paganotfound
+  { path: '**', component: NopagesfoundComponent}
 
 ];
 
